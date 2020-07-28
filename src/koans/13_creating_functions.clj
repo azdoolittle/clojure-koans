17d10b7d5797c3736609d4fc800709183c072b62
azdoolittle <drewdoolittle@gmail.com>
2020-07-28 09:44:48 -0400

    koans up to 13 - creating fns

--- src/koans/13_creating_functions.clj
+++ src/koans/13_creating_functions.clj
@@ -5,31 +5,42 @@
 
 (meditations
   "One may know what they seek by knowing what they do not seek"
-  (= [__ __ __] (let [not-a-symbol? (complement symbol?)]
-                  (map not-a-symbol? [:a 'b "c"])))
+  (= [true false true] (let [not-a-symbol? (complement symbol?)]
+                         (map not-a-symbol? [:a 'b "c"])))
+  ;; only 'b is a symbol, but complement return the opposite
 
   "Praise and 'complement' may help you separate the wheat from the chaff"
   (= [:wheat "wheat" 'wheat]
-       (let [not-nil? ___]
+       (let [not-nil? (complement nil?)]
          (filter not-nil? [nil :wheat nil "wheat" nil 'wheat nil])))
+  ;; similar to previous, complement returns opposite of filter
 
   "Partial functions allow procrastination"
   (= 20 (let [multiply-by-5 (partial * 5)]
-          (___ __)))
+          (multiply-by-5 4)))
+  ;; turns a variable into a fn w/ loose arguments, result becomes argument for next fn call (if any) (hundred-times 3 3) => 900
 
   "Don't forget: first things first"
-  (= [__ __ __ __]
+  (= [:a :b :c :d]
        (let [ab-adder (partial concat [:a :b])]
-         (ab-adder [__ __])))
+         (ab-adder [:c :d])))
+  ;; via partial, ab-adder becomes concat
 
   "Functions can join forces as one 'composed' function"
   (= 25 (let [inc-and-square (comp square inc)]
-          (inc-and-square __)))
+          (inc-and-square 4)))
+  ;; output of fn => input for next fn
+  ;; (defn f [x] (+ 11 (- x 90)))
+  ;; (defn g [x] (* 2 x))
+  ;; ((comp g f) 100) => (* 21 2) => 42
 
   "Have a go on a double dec-er"
-  (= __ (let [double-dec (comp dec dec)]
+  (= 8 (let [double-dec (comp dec dec)]
           (double-dec 10)))
+  ;; dec(rement by 1)
 
   "Be careful about the order in which you mix your functions"
-  (= 99 (let [square-and-dec ___]
-          (square-and-dec 10))))
+  (= 99 (let [square-and-dec (comp dec square)]
+          (square-and-dec  10 )))
+  ;; squares input, then decrements
+  )
